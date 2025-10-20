a10f85ffa912f4ea561e10752ef8e7126d9f243b
azafar <azafar17@my.centennialcollege.ca>
2025-10-20 10:21:41 -0400

    added mongo/mongoose frameworks to create proper backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import MainRouter from '../MainRouter.jsx';
-const App = () => {
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
 return (
-<Router>
-
-        {/* Creates nav bar that is visible in all webpages*/}
-        <nav>
-          <div className="space-x-6">
-            <Link to="/">Home</Link>
-            <Link to="/About">About Me</Link>
-            <Link to="/Education">Education</Link>
-            <Link to="/Project">Projects</Link>
-            <Link to="/Services">Services</Link>
-            <Link to="/Contact">Contact</Link>
-          </div>
-        </nav>
-
-       
-
-<MainRouter />
-</Router>
-);
-};
-export default App;
+  <>
+    <div>
+        <h1>Welcome to Frontend</h1>
+    </div>
+  </>
+    )
+}
+export default App
